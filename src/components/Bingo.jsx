86e7fac0ffa8078c23383daa0fedef528b296b81
alazar <alazarfikadu15@gmail.com>
2025-04-05 17:03:27 +0300

    update the functions and integrate them with socket.io

--- src/components/Bingo.jsx
+++ src/components/Bingo.jsx
@@ -5,7 +5,7 @@ import { useNavigate, useSearchParams } from "react-router-dom";
 import { io } from "socket.io-client";
 
 // Initialize socket connection
-const socket = io("http://localhost:5000"); // Change to your backend address 
+const socket = io("https://bingobot-backend.onrender.com"); // Change to your backend address 
 
 function Bingo (){
 
