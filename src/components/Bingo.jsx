7d1f24f7783b8e8f1bea6caf1cbd6fde7b148b34
alazar <alazarfikadu15@gmail.com>
2025-04-09 14:46:11 +0300

    fix the the card dsplay for the new users

--- src/components/Bingo.jsx
+++ src/components/Bingo.jsx
@@ -231,10 +231,11 @@ function Bingo() {
       </div>
 
       <div className="grid grid-cols-10 gap-1 p-2 max-w-lg w-full text-xs">
-  {numbers.map((num) => {
+   {numbers.map((num) => {
     const isMyCard = cartelaId === num;
-     const isOtherCard = Object.values(otherSelectedCards).includes(num);
+    //  const isOtherCard = Object.values(otherSelectedCards).includes(num);
     //const isOtherCard = (num) => Object.keys(otherSelectedCards).includes(num.toString());
+    const isOtherCard = otherSelectedCards.hasOwnProperty(num.toString());
 
     return (
       <button
