37dfe55a9e24e1bccfc960507cb17675ef262c2e
MikeWeDev <mikyasnegash2@gmail.com>
2025-06-23 23:23:17 +0300

    fully functional nav with its according pages2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import React , { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Bingo from "./components/Bingo";
 import BingoGame from "./components/nextpg";
