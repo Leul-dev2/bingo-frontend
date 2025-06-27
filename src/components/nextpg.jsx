ec35135d16c1698a5f5bc19e9285ddbe17d16b35
MikeWeDev <mikyasnegash2@gmail.com>
2025-06-27 15:35:09 +0300

    fixing the next page leave button problem which persist the card that sellected before

--- src/components/nextpg.jsx
+++ src/components/nextpg.jsx
@@ -29,6 +29,19 @@ const BingoGame = () => {
 
   const hasJoinedRef = useRef(false);
 
+
+
+
+  ////////////////////////////////////////////////////////////////////////////////
+
+  const handleLeave = () => {
+  sessionStorage.removeItem("mySelectedCardId"); // Clear the selected card
+  navigate("/"); // Navigate back to home page
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
   // 1️⃣ Connect socket and joinGame
   useEffect(() => {
     if (!socket.connected) {
@@ -450,7 +463,7 @@ useEffect(() => {
           <button className="w-1/2 bg-blue-500 px-4 py-2 text-white rounded-lg text-lg">
             Refresh
           </button>
-          <button onClick={() => navigate("/")} className="w-1/2 bg-green-500 px-4 py-2 text-white rounded-lg text-lg">
+          <button onClick={handleLeave} className="w-1/2 bg-green-500 px-4 py-2 text-white rounded-lg text-lg">
             Leave
           </button>
         </div>
