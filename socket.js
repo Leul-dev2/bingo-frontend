d4dab85b39ee1b0df7baf5fd1a80e07bc50249de
alazar <alazarfikadu15@gmail.com>
2025-08-31 22:27:54 +0300

    import ✅ Shared socket instance an d fix the path

--- socket.js
+++ socket.js
@@ -4,14 +4,4 @@ const socket = io("https://api.bingoogame.com", {
   transports: ["websocket"],
 });
 
-socket.on("connect", () => {
-  console.log("✅ Connected:", socket.id);
-});
-
-socket.on("connect_error", (err) => {
-  console.error("❌ Connection error:", err.message);
-});
-
-socket.on("disconnect", (reason) => {
-  console.log("⚠️ Disconnected:", reason);
-});
+export default socket;
